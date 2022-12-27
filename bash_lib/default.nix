4d52dcd158e992d52ed2dc955db0159fa9a86527
mstream <maciej.laciak@gmail.com>
2022-12-27 10:09:48 +0100

    add comments to the bash_lib Nix expression

--- bash_lib/default.nix
+++ bash_lib/default.nix
@@ -1,14 +1,23 @@
-{ makeWrapper, stdenv, ... }:
+{ stdenv, ... }:
 
 stdenv.mkDerivation {
+  /*
+    Create the binary directory and copy the source script.
+    The 'fixup' phase will make sure that the shebang is substituted
+    for a Bash interpreter coming from the Nix store.
+    The out variable refers to a path in Nix store where a package would
+    live if derivation was to be realised (via 'nix-store --realise' or
+    'nix build').
+  */
   installPhase = ''
     mkdir -p $out/bin
     cp $src/sayHello.sh $out/bin/sayHello.sh
   '';
   name = "bash-lib";
-  nativeBuildInputs = [ makeWrapper ];
-  postFixup = ''
-    wrapProgram $out/bin/sayHello.sh
-  '';
+  /*
+    Source files directory. Files inside it are treated as inputs to 
+    the derivation and influence its hash. This directory can be referred
+    in phase hooks via 'src' environmental variable.
+  */
   src = ./src;
 } 
