f41b0d4d7844c7df1314e5a1b27aa154c51295f6
mstream <maciej.laciak@gmail.com>
2022-12-29 16:20:42 +0100

    add a Nix code linter

--- flake.nix
+++ flake.nix
@@ -22,11 +22,19 @@
       flake = false;
       url = "github:justinwoo/spago2nix";
     };
+    statix.url = "github:nerdypepper/statix";
   };
   /*
     A function which accepts inputs and produces one or many outputs.
   */
-  outputs = { easy-purescript-nix, flake-utils, nixpkgs, spago2nix, ... }:
+  outputs =
+    { easy-purescript-nix
+    , flake-utils
+    , nixpkgs
+    , spago2nix
+    , statix
+    , ...
+    }:
     let
       supportedSystems = [
         "aarch64-darwin"
@@ -35,70 +43,71 @@
       ];
     in
     flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        pkgs = import nixpkgs
-          { inherit system; };
-        easy-ps = import easy-purescript-nix
-          { inherit pkgs; };
-        bashLib = pkgs.callPackage ./bash_lib/default.nix
-          { };
-        buildConf = import ./build_conf/default.nix;
-        nodeLib = (import ./node_lib/default.nix
-          { inherit pkgs; nodejs = pkgs.nodejs; }
-        ).package;
-        spago-pkgs = import ./purescript_lib/spago-packages.nix
-          { inherit pkgs; };
-        purescriptLib = pkgs.callPackage ./purescript_lib/default.nix
-          {
-            inherit spago-pkgs;
-            purs = easy-ps.purs;
-            spago = easy-ps.spago;
-          };
-        runtimeConf = pkgs.callPackage ./runtime_conf/default.nix
-          { };
-        hello = pkgs.callPackage ./hello/default.nix
-          { inherit bashLib buildConf nodeLib pkgs purescriptLib runtimeConf; };
-      in
-        /* 
-          We produce one default app output per system,
-          which is built from the expression of the app/default.nix,
-          but potentially flake can provide many of them.
-          The 'apps' attribute means that its entries contain executables 
-          that `nix run` command can run. The 'default' attribute means
-          that that if we do not provide any application name for that 
-          command, the application assigned to it will be executed.
-          On top of that, we expose the hello application and libraries 
-          it depends on as package outputs so we can easily build them
-          individually if we want.
-          To examine all the outputs produced by this flake, we can run
-          `nix flake show` from the directory that this flake resides.
-        */
-      {
-        apps.default = flake-utils.lib.mkApp {
-          drv = hello;
-          name = "hello.sh";
+    let
+      pkgs = import nixpkgs
+        { inherit system; };
+      easy-ps = import easy-purescript-nix
+        { inherit pkgs; };
+      bashLib = pkgs.callPackage ./bash_lib/default.nix
+        { };
+      buildConf = import ./build_conf/default.nix;
+      nodeLib = (import ./node_lib/default.nix
+        { inherit pkgs; nodejs = pkgs.nodejs; }
+      ).package;
+      spago-pkgs = import ./purescript_lib/spago-packages.nix
+        { inherit pkgs; };
+      purescriptLib = pkgs.callPackage ./purescript_lib/default.nix
+        {
+          inherit spago-pkgs;
+          purs = easy-ps.purs;
+          spago = easy-ps.spago;
         };
-        /*
-          DevShell allows to spawn a Bash shell with a customized setup.
-          Here, we set it up with all inputs required by the 'hello' 
-          application and its libraries. On top of that, a Git package.
-          The shell is run with `nix develop` command. It is also 
-          possible to make direnv load it automatically.  
-        */
-        devShell = pkgs.mkShell {
-          inputsFrom = [ bashLib hello nodeLib purescriptLib ];
-          nativeBuildInputs = with pkgs; [
-            nix-prefetch-git
-            nodejs
-            nodePackages.node2nix
-            git
-            (import spago2nix { inherit pkgs; })
-          ];
-        };
-        packages = {
-          inherit bashLib nodeLib purescriptLib;
-          default = hello;
-        };
-      }
+      runtimeConf = pkgs.callPackage ./runtime_conf/default.nix
+        { };
+      hello = pkgs.callPackage ./hello/default.nix
+        { inherit bashLib buildConf nodeLib pkgs purescriptLib runtimeConf; };
+    in
+      /* 
+        We produce one default app output per system,
+        which is built from the expression of the app/default.nix,
+        but potentially flake can provide many of them.
+        The 'apps' attribute means that its entries contain executables 
+        that `nix run` command can run. The 'default' attribute means
+        that that if we do not provide any application name for that 
+        command, the application assigned to it will be executed.
+        On top of that, we expose the hello application and libraries 
+        it depends on as package outputs so we can easily build them
+        individually if we want.
+        To examine all the outputs produced by this flake, we can run
+        `nix flake show` from the directory that this flake resides.
+      */
+    {
+      apps.default = flake-utils.lib.mkApp {
+        drv = hello;
+        name = "hello.sh";
+      };
+      /*
+        DevShell allows to spawn a Bash shell with a customized setup.
+        Here, we set it up with all inputs required by the 'hello' 
+        application and its libraries. On top of that, a Git package.
+        The shell is run with `nix develop` command. It is also 
+        possible to make direnv load it automatically.  
+      */
+      devShell = pkgs.mkShell {
+        inputsFrom = [ bashLib hello nodeLib purescriptLib ];
+        nativeBuildInputs = with pkgs; [
+          git
+          nix-prefetch-git
+          nodejs
+          nodePackages.node2nix
+          statix
+          (import spago2nix { inherit pkgs; })
+        ];
+      };
+      packages = {
+        inherit bashLib nodeLib purescriptLib;
+        default = hello;
+      };
+    }
     );
 }
