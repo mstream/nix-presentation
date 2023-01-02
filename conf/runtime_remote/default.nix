189933bf9f8407c796a50c271a4c2dc9d14fa0f4
mstream <maciej.laciak@gmail.com>
2023-01-02 10:30:06 +0000

    update the remote runtime configuration script location

--- conf/runtime_remote/default.nix
+++ conf/runtime_remote/default.nix
@@ -9,11 +9,11 @@ stdenv.mkDerivation {
   inherit name;
   installPhase = ''
     mkdir -p $out/bin
-    cp $src/files/loadConf.sh $out/bin/loadConf.sh
+    cp $src/downloadable_files/loadConf.sh $out/bin/loadConf.sh
   '';
   src = fetchgit {
     url = "https://github.com/mstream/nix-presentation";
-    rev = "e9850e98b370692a980e1e3378df201b71b0b53e";
-    sha256 = "sha256-iNI+CvLT9f8KedUcfFLUFQrYWjagaADqlaXutUwzomI=";
+    rev = "77cb41438c752f454924852f6d65abc892898328";
+    sha256 = "sha256-KTHyxTgZCS104kYhhuSK1HPLtySF+B/SrOvLeDvig+g=";
   };
 } 
