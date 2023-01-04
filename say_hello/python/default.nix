8da5debf283be6a968b5b77df3ebdf6926ec900a
Maciej Laciak <maciej.laciak@gmail.com>
2023-01-04 15:50:59 +0000

    add a dependency to the python library (#11)

--- say_hello/python/default.nix
+++ say_hello/python/default.nix
@@ -6,42 +6,32 @@ let
   pythonMajorVer = "3";
   pythonMinorVer = "9";
   pythonPackage = pkgs."python${pythonMajorVer}${pythonMinorVer}";
-  compositionPackage = buildPythonApplication {
+  pythonApp = buildPythonApplication {
     python = "python${pythonMajorVer}${pythonMinorVer}";
     src = ./.;
   };
-  libDir = "${compositionPackage}/lib/python${pythonMajorVer}.${pythonMinorVer}/site-packages";
   name = "say-hello-python";
 in
 pkgs.stdenv.mkDerivation {
   inherit name;
-  buildInputs = [ pythonPackage ];
+  unpackPhase = "true";
+  buildInputs = [ pythonApp ];
   checkPhase = ''
     pytest 
   '';
-  /* 
-    Make the check phase execute.
-  */
-  doCheck = true;
+  doCheck = false;
   installPhase = ''
     mkdir -p $out/bin
-    cp app.py $out/
-    cp hello.py $out/
   '';
   nativeBuildInputs = with pkgs; [
     makeWrapper
-    pythonPackage
     pkgs."python${pythonMajorVer}${pythonMinorVer}Packages".pytest
   ];
   postFixup = ''
     makeWrapper \
       ${pythonPackage}/bin/python \
       $out/bin/${name} \
-      --add-flags "$out/app.py"
+      --add-flags "${pythonApp}/lib/python${pythonMajorVer}.${pythonMinorVer}/site-packages/app.py"
   '';
   src = ./.;
-  unpackPhase = ''
-    cp -r ${libDir}/*.py .
-  '';
 }
-
